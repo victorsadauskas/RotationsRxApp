ec78380a6e4d58b00febf0be38aa66de8f111a62
Victor <victor.sadauskas@gmail.com>
2016-02-14 12:04:27 -0600

    Deleted migration

--- db/migrate/20160205204600_add_fields_to_rotations.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToRotations < ActiveRecord::Migration
-  def change
-    add_column :rotations, :hours, :integer
-    add_column :rotations, :location, :text
-    add_column :rotations, :duration, :string
-    add_column :rotations, :text, :string
-  end
-end
