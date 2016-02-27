fa891d3c23cb0cb23947f3657fedec37bd30430c
Victor <victor.sadauskas@gmail.com>
2016-02-27 16:35:58 -0600

    Fixing formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,34 @@
 module ApplicationHelper
 
-def markdown(text)
-	 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
-		no_intra_emphasis: true, 
-	    fenced_code_blocks: true,   
-	    disable_indented_code_blocks: true,
-	    hard_wrap: true,
-	    autolink: true,
-	    filter_html: true)
-	markdown.render(text).html_safe
-end
+# def markdown(text)
+# 	#  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
+# 	# 	no_intra_emphasis: true, 
+# 	#     fenced_code_blocks: true,   
+# 	#     disable_indented_code_blocks: true)
+# 	# return markdown.render(text).html_safe
+
+# end
+
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true, 
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true, 
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 
 
 
